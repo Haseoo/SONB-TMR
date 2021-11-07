a455a99855fd51c78fb7214c3859910d0fc3fc74
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2021-11-07 23:06:57 +0100

    improve voter component

--- src/main/java/pl/kielce/tu/weaii/sonb/tmr/circuit/CircuitComponent.java
+++ src/main/java/pl/kielce/tu/weaii/sonb/tmr/circuit/CircuitComponent.java
@@ -4,10 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import pl.kielce.tu.weaii.sonb.tmr.main.MainController;
 
 import java.io.IOException;
 
