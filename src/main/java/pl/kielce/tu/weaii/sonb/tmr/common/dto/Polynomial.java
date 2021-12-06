6e7c1843caca01aebcad65e4d0f6942ffd333dcf
mateo <mateuszwojcik@maczek.local>
2021-12-06 22:53:22 +0100

    Aaaaaa

--- src/main/java/pl/kielce/tu/weaii/sonb/tmr/common/dto/Polynomial.java
+++ src/main/java/pl/kielce/tu/weaii/sonb/tmr/common/dto/Polynomial.java
@@ -5,14 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class Polynomial {
-    private ArrayList<Integer> coefficients;
+    private List<Integer> coefficients;
 
     @JsonCreator
-    public Polynomial(ArrayList<Integer> coefficients) {
+    public Polynomial(List<Integer> coefficients) {
         this.coefficients = coefficients;
     }
 
@@ -38,11 +40,60 @@ public class Polynomial {
         return sbuilder.toString();
     }
 
+    public double func(double x){
+        double result = 0;
+        for(int i=1; i<= coefficients.size() - 1; i++){
+            result += Math.pow(x,i)*coefficients.get(i);
+        }
+        return result + coefficients.get(0);
+    }
+
+    static final float EPSILON = (float)0.01;
+
+    public Integer bisection(double a, double b)
+    {
+        if (func(a) * func(b) >= 0)
+        {
+            System.out.println("You have not assumed"
+                    + " right a and b");
+            return null;
+        }
+
+        double c = a;
+        while ((b-a) >= EPSILON)
+        {
+            // Find middle point
+            c = (a+b)/2;
+
+            // Check if middle point is root
+            if (func(c) == 0.0)
+                break;
+
+                // Decide the side to repeat the steps
+            else if (func(c)*func(a) < 0)
+                b = c;
+            else
+                a = c;
+        }
+
+
+        return Math.toIntExact(Math.round(c));
+    }
+
     @Override
     public String toString() {
         return "Polynomial{" +
                 "coefficients=" + coefficients +
                 ", expression='" + buildExpression() + '\'' +
                 '}';
+    }
+
+    public static void main(String[] args) {
+        Polynomial polynomial = new Polynomial();
+
+        polynomial.setCoefficients(Arrays.asList(-144,0,1));
+
+        System.out.println(Integer.toBinaryString(polynomial.bisection(0, 255)));
+
     }
 }
